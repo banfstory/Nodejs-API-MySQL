ecd9908f22e1e822fd175a3526e6637d5b4228dc
banfstory <denis_chi_yiu@hotmail.com>
2021-08-06 22:13:45 +1000

    Added initialization of database

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+// https://github.com/mysqljs/mysql
+const mysql = require('mysql');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World');
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'forum_db'
 });
+
+db.connect(err => {
+    if(err) throw err;
+    console.log('MySql Connected');
+});
+
+const database_init = require('./routes/database_init.js');
+app.use((req, res, next) => {
+    res.locals.db = db;
+    next();
+});
+app.use('/database', database_init);
 
 app.listen(5000, () => {
     console.log('Listening on port 5000');
---
+++ routes/database_init.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+// create database (remove property of 'database' to execute this code)
+router.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE forum_db';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('DB added');
+    });
+});
+
+// create user table (execute create table first)
+router.get('/createusertable', (req, res) => {
+    let sql = 'CREATE TABLE user(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(100), email VARCHAR(100),  password VARCHAR(100))';
+    res.locals.db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('User table created');
+    }); 
+});
+
+// create forum table (execute create table second)
+router.get('/createforumtable', (req, res) => {
+    let sql = 'CREATE TABLE forum(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), about VARCHAR(100), userId int NOT NULL, FOREIGN KEY(userId) REFERENCES user(id))';
+    res.locals.db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Forum table created');
+    }); 
+});
+
+// create post table (execute create table last)
+router.get('/createposttable', (req, res) => {
+    let sql = 'CREATE TABLE post(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(100), content VARCHAR(100), userId int NOT NULL, forumId int NOT NULL, FOREIGN KEY(userId) REFERENCES user(id), FOREIGN KEY(forumId) REFERENCES forum(id))';
+    res.locals.db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Forum table created');
+    }); 
+});
+
+module.exports = router;
